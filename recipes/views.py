a08285e4d063e5aef742f8bd285b3e6a95e9ee3c
André Bueno <andre.luiz.silva.bueno@hotmail.com>
2025-07-27 09:48:39 -0300

    Modulo 5 - Aula 14. A função render e mais sobre caminhos de templates  - Exemplos de Template temp

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'André Luiz'
+    })
+
+def temp(request):
+    return render(request, 'apagar/temp.html')
 
 def sobre(request):
     return HttpResponse('Sobre - Pagina Sobre')
